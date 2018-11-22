516a5c3a50b6904aae8c39d4f40f3ddc57b13a33
Anuitex <Anuitex@DESKTOP-00VRJM6>
2018-11-22 20:37:57 +0200

    CreateDAL

---
+++ DataAccessLayer/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+
+        public UserRole UserRole { get; set; }
+    }
+
+    public enum UserRole
+    {
+        None = 0,
+        BotPlayer = 1,
+        PeoplePlayer = 2,
+        Dealer = 3
+    }
+}
